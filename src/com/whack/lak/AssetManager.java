0069ce25090939fd3f317f5794a8c22c3ab5107f
SELENA1405 <selena141ss@gmail.com>
2025-03-31 22:21:23 +0700

    Update AssetManager.java

--- src/com/whack/lak/AssetManager.java
+++ src/com/whack/lak/AssetManager.java
@@ -1,4 +1,25 @@
-public class AssetManager {
 
-    print.lm
+    package assets;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class AssetManager {
+    private static final Map<String, BufferedImage> images = new HashMap<>();
+
+    public static BufferedImage loadImage(String path) {
+        if (!images.containsKey(path)) {
+            try {
+                images.put(path, ImageIO.read(Objects.requireNonNull(AssetManager.class.getResource(path))));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return images.get(path);
+    }
 }
+
