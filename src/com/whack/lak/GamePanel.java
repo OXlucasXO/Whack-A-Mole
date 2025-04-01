b7f47526304b5ef6e09559584015d1e64cbad23f
OXlucasXO <85760779+OXlucasXO@users.noreply.github.com>
2025-04-01 11:45:50 +0700

    lib import

--- src/com/whack/lak/GamePanel.java
+++ src/com/whack/lak/GamePanel.java
@@ -1,6 +1,21 @@
-public class GamePanel {
-    public static void main(String[] args) {
-        
+import processing.core.PApplet;
+
+public class GamePanel extends PApplet {
+
+    public void settings() {
+        size(800, 600); // Set window size
     }
 
+    public void setup() {
+        background(200);
+    }
+
+    public void draw() {
+        fill(255, 0, 0);
+        ellipse(mouseX, mouseY, 50, 50); // Draw a circle following the mouse
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("com.whack.lak.GamePanel");
+    }
 }
