1a56a459a72f0deb6b96caedeb7fcf12520191cc
Konyi <konyi.kn800@gmail.com>
2025-04-20 12:19:53 +0700

    Revert "combined ScoreTimerManager and SpawnManager"
    This reverts commit 9b33c5d60551fc38a31d1a949d6a2307a3565a20.

--- src/main/java/com/whack/lak/Main.java
+++ src/main/java/com/whack/lak/Main.java
@@ -4,7 +4,8 @@ import processing.core.PApplet;
 
 public class Main extends PApplet {
 
-    
+    SpawnManager spawnManager;
+    MouseHandler mouseHandler; // Declare MouseHandler
 
     public void settings() {
         fullScreen(FX2D);
@@ -18,24 +19,20 @@ public class Main extends PApplet {
 
        spawnManager = new SpawnManager(this);
 
-        scoreTimerManager = new ScoreTimerManager(this);
-        
+        circle(200, 200, 100);
+        spawnManager.display();
+
+        mouseHandler = new MouseHandler(this, spawnManager); // Initialize MouseHandler
     }
 
     public void draw() {
-        if (keyPressed) {
-            if (key == 'R' || key == 'r') {
-            // background(0);
-            // SpawnManager manager = new SpawnManager(this);
-            // manager.display();
-            
-            }
+        background(124,252,0,100);
+        spawnManager.display();
 
-          }
-        background(255);
-        scoreTimerManager.update();
-        spawnManager.update();
-        
+        if (keyPressed && (key == 'r' || key == 'R')) {
+            background(124,252,0,100);
+            spawnManager.resetGame();
+        }
     }
 
     public void mousePressed() {
